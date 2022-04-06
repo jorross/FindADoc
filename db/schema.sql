60eaffae4db806d09f20a03ea42e5aece6162477
ItzHefe <jeffrey.jarvis32@gmail.com>
2022-04-06 18:02:29 -0400

    fixed class export for Doctor, showing properly in all file snow

--- db/schema.sql
+++ db/schema.sql
@@ -3,21 +3,21 @@ CREATE DATABASE doctor_db;
 
 USE doctor_db;
 
-CREATE TABLE doctors (
-    id INT AUTO_INCREMENT NOT NULL,
-    doctor_name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
-);
+-- CREATE TABLE doctors (
+--     id INT AUTO_INCREMENT NOT NULL,
+--     doctor_name VARCHAR(30) NOT NULL,
+--     PRIMARY KEY (id)
+-- );
 
-CREATE TABLE user (
-    id INT AUTO_INCREMENT NOT NULL,
-    user_name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
-);
+-- CREATE TABLE user (
+--     id INT AUTO_INCREMENT NOT NULL,
+--     user_name VARCHAR(30) NOT NULL,
+--     PRIMARY KEY (id)
+-- );
 
-CREATE TABLE appointment (
-    id INT AUTO_INCREMENT NOT NULL,
-    PRIMARY KEY (id)
-    doctor_id FOREIGN KEY (id),
-    user_id FOREIGN KEY (id)
-);
+-- CREATE TABLE appointment (
+--     id INT AUTO_INCREMENT NOT NULL,
+--     PRIMARY KEY (id)
+--     doctor_id FOREIGN KEY (doctors.id),
+--     user_id FOREIGN KEY (user.id)
+-- );
