4ddde6b5fb81180054506804e61e0e0ec2c07e1e
Jordan Ross <jorross@umich.edu>
2022-04-09 10:20:33 -0700

    creating new user via signup api route

--- public/js/signup.js
+++ public/js/signup.js
@@ -6,7 +6,7 @@ const signupFormHandler = async (event) => {
   const password = document.querySelector("#password-signup").value.trim();
 
   if (name && email && password) {
-    const response = await fetch("/api/users", {
+    const response = await fetch("/api/users/signup", {
       method: "POST",
       body: JSON.stringify({ name, email, password }),
       headers: { "Content-Type": "application/json" },
